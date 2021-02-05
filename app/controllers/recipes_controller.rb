80168a3630c0cc11ba12b2471099369350a21faa
Hiroyuk Ishizuka <hiroyuki.1207.18@gmail.com>
2021-02-05 10:01:15 +0900

    [create]recipes/index,show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,28 @@
 class RecipesController < ApplicationController
   def index
+  	@recipes = Recipe.all
   end
 
   def show
+  	@recipe = Recipe.find(params[:id])
   end
 
   def new
+  	@recipe = Recipe.new
+  end
+
+  def create
+  	@recipe = Recipe.new(recipe_params)
+  	@recipe.user_id = current_user.id
+  	@recioe.save
+  	redirect_to recipe_path(@recipe)
   end
 
   def edit
+  end
+
+  private
+  def recipe_params
+  	params.require(:recipe).permit(:title :body :image)
   end
 end
